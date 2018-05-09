04379aa2d7ebc4aa830412c92886511ea34fd4ea
Daniel Rubens <dan.rubens@gmail.com>
2018-05-09 02:39:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {bobDylan: 'tangledUpInBlue'};
 
-function updatePlaylist(obj, key, value) {
+function updatePlaylist(obj, artistName, songTitle) {
 
   return Object.assign({}, playlist, { [artistName]: songTitle })
 }
